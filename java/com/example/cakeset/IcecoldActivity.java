c92e8d1dcd00a43b4747cf59c6ec1ef0daac0b56
Abidluayraihantaufik <spaskeren30@gmail.com>
2023-11-26 11:19:29 +0700

    first commit

---
+++ java/com/example/cakeset/IcecoldActivity.java
@@ -0,0 +1,14 @@
+package com.example.cakeset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IcecoldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icecold);
+    }
+}
